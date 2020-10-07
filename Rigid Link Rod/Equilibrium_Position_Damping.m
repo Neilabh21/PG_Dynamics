4802467d97ef6f4764a77b297e9a0d7b35fc72e6
Neilabh Banzal <34180039+Neilabh21@users.noreply.github.com>
2020-10-07 17:13:22 +0530

    Update Eqiulibrium_Position.m
    Update Eqiulibrium_Position.m - apply corrections to the equations.

--- Rigid Link Rod/Equilibrium_Position_Damping.m
+++ Rigid Link Rod/Equilibrium_Position_Damping.m
@@ -1,4 +1,4 @@
-function [x, fval] = Equilibrium_Position()
+function [x, fval] = Equilibrium_Position_Damping()
 % Equilibrium position of a beam
 %   The rigid-rod torsion spring model is used to model the beam and
 %   calculate the equilibrium position.
@@ -75,14 +75,6 @@ if (n_links > 2)
 end
 
 F(n_links) = k * (x(n_links) - x(n_links - 1)) - g * l * sin(x(n_links)) * (M + m * 0.5);
-
-% F(1) = k * (abs(x(1)) - abs(x(2))) - g * l * sin(abs(x(1))) * (M + m * (n_links - 0.5));
-% 
-% for i_1 = 2:(n_links-1)
-%     F(i_1) = k * (-abs(x(i_1-1)) + 2 * abs(x(i_1)) - abs(x(i_1+1))) - g * l * sin(abs(x(i_1))) * (M + m * (n_links - i_1 + 0.5));
-% end
-% 
-% F(n_links) = k * (abs(x(n_links)) - abs(x(n_links - 1))) - g * l * sin(abs(x(n_links))) * (M + m * 0.5);
 
 end
 end
